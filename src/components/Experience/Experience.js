ab117151cdf9512bb0e336b5c354ab45f275235b
Gabriel <gabo@Gabriels-MacBook-Pro.local>
2022-06-01 19:40:32 +0200

    updated components in order to recive parameters

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -1,13 +1,11 @@
 import React from 'react';
-
-import ResumeData from '../Resume/ResumeData.json';
 import './Experience.scss';
 
-const Experience = () => {
+const Experience = ({works}) => {
   return (
     <div className="__work">
       <h3>Experience</h3>
-      {ResumeData.work.map((item, index) => {
+      {works.map((item, index) => {
         return (
           <ul className="work__experience" key={index}>
             <div className="date">
