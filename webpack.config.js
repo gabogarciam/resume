84558dd70c0728968c2dba86cba803c8e9ab8469
gabogarciam <gabogarciam71@gmail.com>
2022-06-02 19:31:29 +0200

    fix script, added env parameter to webpack, added package-lock.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,50 @@
 const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
-const TerserPlugin = require("terser-webpack-plugin");
 
-module.exports = {
-  entry: {
-    main: './src/App.js',
-  },
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: 'html-loader',
+module.exports = (env, argv) => {
+  const prod = argv.mode === 'production';
+
+  return {
+    entry: {
+      main: './src/App.js',
+    },
+    output: {
+      path: path.join(__dirname, 'dist'),
+      filename: 'bundle.js',
+    },
+    devtool: 'source-map',
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
           },
-        ],
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: ['file-loader'],
-      },
+        },
+        {
+          test: /\.html$/,
+          use: [
+            {
+              loader: 'html-loader',
+            },
+          ],
+        },
+        {
+          test: /\.s[ac]ss$/i,
+          use: ['style-loader', 'css-loader', 'sass-loader'],
+        },
+        {
+          test: /\.(png|svg|jpg|gif)$/,
+          use: ['file-loader'],
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebPackPlugin({
+        template: './public/index.html',
+        filename: './index.html',
+      }),
     ],
-  },
-  optimization: {
-    minimize: true,
-    minimizer: [new TerserPlugin()],
-  },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: './public/index.html',
-      filename: './index.html',
-    }),
-  ],
+  }
 };
