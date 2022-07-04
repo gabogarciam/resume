1247f8feed9155a982f7a10a6f5309e926c5de57
gabogarciam <gabogarciam71@gmail.com>
2022-07-04 19:23:42 +0200

    refactor: :art: improve the structure folder styles
    improve the structure folder styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import Resume from './components/Resume/Resume';
-import './App.scss';
+import './styles/global.scss';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
