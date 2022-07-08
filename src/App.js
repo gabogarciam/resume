779bf21cf1cda8b554dd7695cd2dd112230fad1c
gabogarciam <gabogarciam71@gmail.com>
2022-07-08 15:44:25 +0200

    refactor: :art: update import
    update import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import Resume from './components/Resume/Resume';
+import Resume from './components/Resume';
 import './styles/global.scss';
 
 createRoot(document.getElementById('root')).render(
